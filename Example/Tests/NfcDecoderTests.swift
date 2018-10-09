271fbce903104667f94ef360ea214cc3b90a44f1
Alexander Vasenin <alex.vasenin@me.com>
2018-10-09 03:40:07 +0300

    Add placeholder for NfcDecoderTests

---
+++ Example/Tests/NfcDecoderTests.swift
@@ -0,0 +1,16 @@
+//
+//  NfcDecoderTests.swift
+//  NfcDecoder_Tests
+//
+//  Created by Alexander Vasenin on 09/10/2018.
+//  Copyright © 2018 Impekable LLC. All rights reserved.
+//
+
+import XCTest
+@testable import NfcDecoder
+
+class NfcDecoderTests: XCTestCase {
+    
+}
+
+
