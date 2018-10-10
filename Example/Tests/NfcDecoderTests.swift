517a42928ab5e974e13a9ead5725ef1ba1c63a88
Alexander Vasenin <alex.vasenin@me.com>
2018-10-10 10:34:43 +0300

    Add tests for text and phone number

--- Example/Tests/NfcDecoderTests.swift
+++ Example/Tests/NfcDecoderTests.swift
@@ -12,6 +12,18 @@ import CoreNFC
 
 class NfcDecoderTests: XCTestCase {
     
+    func testDecodeWellKnownText() {
+        let record = NdefRecordMockup(typeNameFormat: .nfcWellKnown, type: Data([0x54]), payload: Data([0x02, 0x65, 0x6e, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]))
+        let payload = NfcDecoder().decode(record)
+        switch payload {
+        case .text(let text):
+            XCTAssertEqual(text.languageCode, "en")
+            XCTAssertEqual(text.text, "Hello, world!")
+        default:
+            XCTFail()
+        }
+    }
+    
     func testDecodeWellKnownUri1() {
         let record = NdefRecordMockup(typeNameFormat: .nfcWellKnown, type: Data([0x55]), payload: Data([0x04, 0x30, 0x74, 0x2e, 0x67, 0x73, 0x2f, 0x52, 0x54, 0x6e, 0x4c, 0x4a, 0x61]))
         let payload = NfcDecoder().decode(record)
@@ -29,6 +41,18 @@ class NfcDecoderTests: XCTestCase {
         switch payload {
         case .uri(let uri):
             XCTAssertEqual(uri.url.absoluteString, "https://0t.gs/MMxXWJ")
+        default:
+            XCTFail()
+        }
+    }
+    
+    func testPhoneNumber() {
+        let rawData = Data([0xD1, 0x01, 0x0D, 0x55, 0x05, 0x2b, 0x33, 0x35, 0x38, 0x39, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37])
+        let record = RawNdefRecord(rawData)
+        let payload = NfcDecoder().decode(record)
+        switch payload {
+        case .uri(let uri):
+            XCTAssertEqual(uri.url.absoluteString, "tel:+35891234567")
         default:
             XCTFail()
         }
