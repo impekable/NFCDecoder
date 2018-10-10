5a8ecd6f446ab1d1d14cc0cdcbb39ac22d3ff13e
Alexander Vasenin <alex.vasenin@me.com>
2018-10-10 05:01:54 +0300

    Add some content to NdefRecord

--- NfcDecoder/Classes/NdefRecord.swift
+++ NfcDecoder/Classes/NdefRecord.swift
@@ -14,7 +14,15 @@ import CoreNFC
  Note: In CoreNFC NDEF record is represented by confusingly named NFCNDEFPayload. Unfortunately we can't just extend NFCNDEFPayload since in has no public initializers (we need for smart posters support).
  */
 
-public struct NdefRecord {
+public struct NdefRecord: CustomStringConvertible {
+    
+    var data: Data
+    
+    public init(_ data: Data) { self.data = data }
+    
+    public init(_ record: NFCNDEFPayload) { self.data = record.payload }
+    
+    public var description: String { return "NdefRecord <\(data.hexEncodedString(separator: " ", every: 4))>" }
     
 }
 
