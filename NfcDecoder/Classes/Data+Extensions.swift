fafe65546c02ab425bc5d38817d19c72e9f8a6b8
Alexander Vasenin <alex.vasenin@me.com>
2018-10-10 04:17:24 +0300

    Reuse Data extensions

---
+++ NfcDecoder/Classes/Data+Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  Data+Extensions.swift
+//  NfcDecoder
+//
+//  Created by Alexander Vasenin on 10/10/2018.
+//  Copyright © 2018 Impekable LLC. All rights reserved.
+//
+
+import Foundation
+
+extension Data {
+    
+    func hexEncodedString() -> String {
+        return map { String(format: "%02hx", $0) } .joined()
+    }
+    
+    func hexEncodedString(separator: String, every: Int) -> String {
+        let indexes = Array(stride(from: startIndex, to: endIndex, by: every))
+        let strings = indexes.map { index -> String in
+            let chunk = subdata(in: index ..< Swift.min(index+every, endIndex))
+            return chunk.hexEncodedString() }
+        return strings.joined(separator: separator)
+    }
+    
+}
+
+
